8722acb8611e60e4f3e942bb7786e04f48409a89
elina-mns <ellie.mansurova@gmail.com>
2020-09-21 15:30:42 -0400

    Initial Commit

---
+++ Random Dog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Dog
+//
+//  Created by Elina Mansurova on 2020-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
